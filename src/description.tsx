b1c4d43fd96d852475ec15cc863a52ecdb482047
aimeudoxie <eudoxieumwali@gmail.com>
2024-03-13 23:45:35 +0200

    adding responsivenes

--- src/description.tsx
+++ src/description.tsx
@@ -1,7 +1,6 @@
 import "./description.css"
 import minus from "./assets/icon-minus.svg"
 import plus from "./assets/icon-plus.svg"
-
 import cart from "./assets/icon-cart.svg"
 const Description=()=>{
     return(
@@ -12,8 +11,8 @@ const Description=()=>{
             <p>These low-profile sneakers are your perfect casual wear 
                 companion.Featuring a durable rubber outer sole, they'll 
                 withstand everything the weather can offer</p>
-            <p><b>$125.00</b> <span id="percentage" >50%</span></p>
-            <p>$250.00</p>
+            <div className="prices"><p><b>$125.00</b><span id="percentage" >50%</span></p>
+            <p className="line-through">$250.00</p></div>
             <div id="cart">
                 <div className="count">
                 <img src={minus} alt="minus icon" className="minus" />
@@ -21,7 +20,7 @@ const Description=()=>{
                 <img src={plus} alt="plus icon" className="plus" />
                 </div>
                 <div id="button">
-                    <img src={cart} alt="cart icon" />
+                    <img src={cart} alt="cart icon" /> 
                     <b>Add to cart</b>
                 </div>
 
--- src/navigation.tsx
+++ src/navigation.tsx
@@ -2,10 +2,12 @@ import "./navigation.css"
 import logo from "./assets/logo.svg"
 import cart from "./assets/icon-cart.svg"
 import avatar from "./assets/image-avatar.png"
+import menu from './assets/icon-menu.svg';
 
-const Navigation = ()=>{
+const Navigation = ()=>{ 
     return(
         <><div id="nav-headers">
+            <div className="burger-menu"><img src={menu} alt="menu" /></div>
         <div className="logo"><img src={logo} alt="logo image"/></div>
          
             <div id="links">
@@ -15,10 +17,10 @@ const Navigation = ()=>{
             <a href="#">About</a>
             <a href="#">Contact</a>
         
-        </div>
+        </div> 
         <div id="profile">
-            <img src={cart}/>
-            <img src={avatar} id="avatar"/>
+            <img src={cart} className="cart"/>
+            <img src={avatar} className="avatar"/>
         </div>
 
         </div>
