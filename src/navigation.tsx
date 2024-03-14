c505be44f82d53565afce6c6bc5296f1b9e3921b
aimeudoxie <eudoxieumwali@gmail.com>
2024-03-14 17:03:24 +0200

    adding cart design

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,30 +1,65 @@
-import "./navigation.css"
-import logo from "./assets/logo.svg"
-import cart from "./assets/icon-cart.svg"
-import avatar from "./assets/image-avatar.png"
+import React, { useState } from 'react';
+import "./navigation.css";
+import logo from "./assets/logo.svg";
+import cart from "./assets/icon-cart.svg";
+import avatar from "./assets/image-avatar.png";
 import menu from './assets/icon-menu.svg';
+import close from './assets/close.png';
+import image1 from './assets/image-product-1.jpg';
+import remove from './assets/delete.png';
 
-const Navigation = ()=>{ 
-    return(
-        <><div id="nav-headers">
-            <div className="burger-menu"><img src={menu} alt="menu" /></div>
-        <div className="logo"><img src={logo} alt="logo image"/></div>
-         
-            <div id="links">
-            <a href="#">Collections</a>
-            <a href="#">Men</a>
-            <a href="#">Women</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
-        
-        </div> 
-        <div id="profile">
-            <img src={cart} className="cart"/>
-            <img src={avatar} className="avatar"/>
-        </div>
+const Navigation = () => {
+    const [showSideLinks, setShowSideLinks] = useState(false);
 
-        </div>
+    const toggleSideLinks = () => {
+        setShowSideLinks(!showSideLinks);
+    };
+    const[ShowCart,setShowCart]=useState(false);
+
+    const toggleCart =() =>{
+        setShowCart(!ShowCart);
+    };
+
+    return (
+        <>
+            <div id="nav-headers">
+                <div className="burger-menu" onClick={toggleSideLinks}>
+                    <img src={menu} alt="menu" />
+                </div>
+                <div className="logo"><img src={logo} alt="logo image" /></div>
+                <div id="links">
+                    <a href="#">Collections</a>
+                    <a href="#">Men</a>
+                    <a href="#">Women</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                </div>
+                <div id="side-links" style={{ display: showSideLinks ? 'flex' : 'none' }}>
+                    <img src={close} alt="close-image" onClick={toggleSideLinks} />
+                    <a href="#">Collections</a>
+                    <a href="#">Men</a>
+                    <a href="#">Women</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                </div>
+                <div id="profile">
+                    <div className="cart"> <img src={cart} onClick={toggleCart}/>
+                    <span className='cart-number'>3</span></div>
+                    <img src={avatar} className="avatar" />
+                </div>
+                <div className="cart-items" style={{ display: ShowCart ? 'flex' : 'none' }} >
+                    <h4>Cart</h4>
+                    <div className="item">
+                        <img src={image1} className="product-Image" />
+                        <p>Fall Limited Sneakers <br/>$125*3 = <b>$375.00</b></p>
+                        <img src={remove} className="delete-icon" />
+                        </div>
+                        <button>Checkout</button>
+                    
+                </div>
+            </div>
         </>
-    )
-}
+    );
+};
+
 export default Navigation;
